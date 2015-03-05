f9a9227fb672f232a0a1f1d6b89445a612396f92
aozora0000 <aozora0000@gmail.com>
2015-03-05 16:12:12 +0900

    add new brewapps

---
+++ .brew.d/anybar.zsh
@@ -0,0 +1,3 @@
+export ANYBAR_PORT=1788
+export ANYBAR_HOST='localhost 1788'
+alias anybar="nc -4u -w0 ${ANYBAR_HOST}"
---
+++ .brew.d/phalcon.zsh
@@ -0,0 +1,4 @@
+export PTOOLSPATH=/Users/aozora0000/Projects/phalcon-devtools/
+export PATH=$PATH:/Users/aozora0000/Projects/phalcon-devtools
+export PTOOLSPATH=/Users/aozora0000/Projects/phalcon-devtools/
+export PATH=$PATH:/Users/aozora0000/Projects/phalcon-devtools
