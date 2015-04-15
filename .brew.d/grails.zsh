86879f53fdc6c30204b6224afdf58e710e386d91
aozora0000 <aozora0000@gmail.com>
2015-04-15 17:29:53 +0900

    delete groovy.zsh grails.zsh add gvm.zsh

--- .brew.d/grails.zsh
+++
@@ -1,3 +0,0 @@
-export GRAILS_HOME=/usr/local/opt/grails/libexec
-export GRAILS_PRJ=~/Projects/grails
-export PATH="${PATH};${GRAILS_HOME}/bin"
---
+++ .brew.d/gvm.zsh
@@ -0,0 +1,1 @@
+[[ -s "/Users/aozora0000/.gvm/bin/gvm-init.sh" ]] && source "/Users/aozora0000/.gvm/bin/gvm-init.sh"
