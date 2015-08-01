cc02c091b047317db54fe421102283ad02752805
aozora0000 <aozora0000@gmail.com>
2015-08-01 22:51:32 +0900

    change brew bundle -> brewdler

---
+++ .brew.d/homebrew.zsh
@@ -0,0 +1,4 @@
+#################################
+# Homebrew設定
+#################################
+[[ -s "$HOME/.HOMEBREW_GITHUB_API_TOKEN" ]] && source "$HOME/.HOMEBREW_GITHUB_API_TOKEN"
--- bundle/bundleinstall.sh
+++ bundle/bundleinstall.sh
@@ -2,7 +2,7 @@
 if [ -z "/usr/local/bin/brew" ];then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-brew tap homebrew/boneyard && \
+brew tap homebrew/brewdler && \
 cd $HOME/.profile/bundle && \
 brew bundle && \
 read-dir `pwd`/package/*_package && \
