c1d302368c806c5e5839b4f1e97c369efeadc996
aozora0000 <aozora0000@gmail.com>
2015-08-04 18:55:13 +0900

    fixed phalcon.zsh

--- .brew.d/phalcon.zsh
+++ .brew.d/phalcon.zsh
@@ -1,4 +1,2 @@
 export PTOOLSPATH=/Users/aozora0000/Projects/phalcon-devtools/
-export PATH=$PATH:/Users/aozora0000/Projects/phalcon-devtools
-export PTOOLSPATH=/Users/aozora0000/Projects/phalcon-devtools/
-export PATH=$PATH:/Users/aozora0000/Projects/phalcon-devtools
+export PATH=$PATH:$PTOOLSPATH
